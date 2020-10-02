df56869585c819587e12e3e4c304288e3dd7c033
Seth Benson <seththebenson@gmail.com>
2020-10-02 10:27:17 -0400

    Add files via upload

--- projectantirun.py
+++ projectantirun.py
@@ -1,59 +1,49 @@
 import pandas as pd
 import fileinput
 import yagmail
-df = pd.read_csv('maillist.csv')
-po = df.POBOX
-email = df.Email
 list = '0123456789/-X'
-def send(e):
-    sender_email = 'seth.benson.test.email@gmail.com'
-    receiver_email = e
-    subject = "PACKAGE"
-    sender_password = 'test123test'
-
-    yag = yagmail.SMTP(user=sender_email, password=sender_password)
-
-    contents = [
-        "Hi",
-        "You have a package",
-    ]
-
-    yag.send(receiver_email, subject, contents)
-def static_run():
-    maxvalue = 0
-    boxnum = input('Enter PO #: ')
-    value = int(input('Enter Size:'))
+place = ''
+size = ''
+pobox = ''
+def remove_run():
     place = ''
-    q = 0
+    size = ''
+    pobox = ''
+    id = input('Enter ID: ')
+    for line in fileinput.input('packagelist.txt', inplace=True):
+        if len(line) > 7 and line[0:6] == id:
+            commacount = 0
+            l = 0
+            while l<len(line):
+                if line[l] == ',':
+                    commacount += 1
+                elif commacount == 1:
+                    pobox = pobox + line[l]
+                elif commacount == 2:
+                    place = place + line[l]
+                elif commacount == 3:
+                    size = size + line[l]
+                l += 1
+        else:
+            print(line, end='')
+    if place == '':
+        print('no package found')
+    else:
+        print(place)
+        print(pobox)
     for line in fileinput.input('Storage.txt', inplace=True):
         c = 0
         row = ''
         while c < len(line) and line[c] in list:
             row += line[c]
             c += 1
-        if len(row) > 3 and row[0] == '0' and row[1] == '0' and row[2] == '0' and row[3] == '0':
-            maxvalue = len(row)
-        if(len(line)+value <= maxvalue + 1 and q == 0):
-            q = 1
-            c = 0
-            while c < value:
-                row += 'X'
-                c += 1
+        if(len(row) > 6 and row[0:6] == place):
+            row = row[0:len(row) - int(size)]
             print(row + '\n', end='')
-            place = row[0:6]
         else:
             print(row + '\n', end='')
-    if place == '':
-        print('no place for the package')
-    else:
-        print(place)
-        n = 0
-        while n < len(po):
-            if int(po[n]) == int(boxnum):
-                send(str(email[n]))
-            n += 1
 
-static_run()
+remove_run()
 
 
 
